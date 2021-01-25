b7564e48af43208aca916a26b474e9034333293e
lidahai <lidahai@5j5j.com>
2021-01-25 19:09:29 +0800

    Init Commit

--- src/main/java/com/imooc/distributedjob/MyJob.java
+++ src/main/java/com/imooc/distributedjob/MyJob.java
@@ -1,5 +1,21 @@
 package com.imooc.distributedjob;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
 public class MyJob {
 
+    // initialDelay 延迟启动
+    // fixedDelay 固定延迟，时间间隔是前次任务的结束到下次任务的开始
+    // fixedRate 固定频率，时间间隔是前次任务和下次任务的开始
+    @Scheduled(fixedRate = 3000, initialDelay = 5000)
+    public void process() throws InterruptedException {
+        log.info("process....start");
+        Thread.sleep(2000);
+        log.info("process....stop");
+    }
 }
