5e68a1df39b077c5486fc303f050c662cc27a7c6
lidahai <lidahai@5j5j.com>
2021-01-26 15:29:32 +0800

    @Async异步执行，不会等待方法执行的过程

--- src/main/java/com/imooc/distributedjob/MyJob.java
+++ src/main/java/com/imooc/distributedjob/MyJob.java
@@ -1,6 +1,8 @@
 package com.imooc.distributedjob;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -9,13 +11,36 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyJob {
 
+    @Autowired
+    private Process process;
+
+
     // initialDelay 延迟启动
     // fixedDelay 固定延迟，时间间隔是前次任务的结束到下次任务的开始
     // fixedRate 固定频率，时间间隔是前次任务和下次任务的开始
-    @Scheduled(cron = "* * * * * ?")
-    public void process() throws InterruptedException {
-        log.info("process....start");
-        Thread.sleep(2000);
-        log.info("process....stop");
+//    @Scheduled(cron = "* * * * * ?")
+//    public void process() throws InterruptedException {
+//        log.info("process....start");
+//        Thread.sleep(2000);
+//        log.info("process....stop");
+//    }
+
+
+    @Scheduled(fixedRate = 3000)
+    public void process1() throws InterruptedException {
+        log.info("process1....start");
+        process.doProcess1();
+        log.info("process1....stop");
+
+    }
+
+//    @Scheduled(fixedRate = 3000)
+//    public void process2() {
+//        log.info("process2....");
+//    }
+
+    @Async
+    public void doProcess1() throws InterruptedException {
+        Thread.sleep(5000);
     }
 }
---
+++ src/main/java/com/imooc/distributedjob/Process.java
@@ -0,0 +1,15 @@
+package com.imooc.distributedjob;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class Process {
+    // 慎用，@Async 多线程的
+    @Async
+    public void doProcess1() throws InterruptedException {
+        Thread.sleep(5000);
+    }
+}
