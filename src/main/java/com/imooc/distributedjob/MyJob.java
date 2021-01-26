8e4dac81f744e7e68a4b9414a837222d30c60f08
lidahai <lidahai@5j5j.com>
2021-01-26 11:35:30 +0800

    @Scheduled注解搭配Cron表达式

--- src/main/java/com/imooc/distributedjob/MyJob.java
+++ src/main/java/com/imooc/distributedjob/MyJob.java
@@ -12,7 +12,7 @@ public class MyJob {
     // initialDelay 延迟启动
     // fixedDelay 固定延迟，时间间隔是前次任务的结束到下次任务的开始
     // fixedRate 固定频率，时间间隔是前次任务和下次任务的开始
-    @Scheduled(fixedRate = 3000, initialDelay = 5000)
+    @Scheduled(cron = "* * * * * ?")
     public void process() throws InterruptedException {
         log.info("process....start");
         Thread.sleep(2000);
